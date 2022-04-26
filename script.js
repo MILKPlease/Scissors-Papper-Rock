82cf90cbdf087ed913122cc23c2dd8e06ab8608c
MILKPlease <lukescrivens444@gmail.com>
2022-04-26 16:01:27 +1000

    Created script for the game

---
+++ script.js
@@ -0,0 +1,36 @@
+const choices = ["rock", "paper", "scissors"];
+
+function game() {
+  // play the game
+  // play 5 rounds
+  // console based
+}
+
+function playRound() {
+  const playerSelction = playerChoice();
+  const computerSelection = computerChoice();
+}
+
+function playerChoice() {
+  let input = prompt("Type Rock, Paper, or Scissors");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  if (check == true) {
+    console.log(input);
+  }
+}
+
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
+  }
+}
